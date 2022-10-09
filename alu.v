ce778db1c5fe051ced8b48776b7b5a7305325735
Syed Omair <syedomair@Syeds-MacBook-Air.local>
2022-10-08 19:04:52 -0500

    add tests for ALU

--- alu.v
+++ alu.v
@@ -1,5 +1,47 @@
 module alu(
-	
+	input [3:0] alu_op,
+	input [31:0] alu_a,
+	input [31:0] alu_b,
+
+	output [31:0] alu_out
 );
+
+`include "riscv_defs.v"
+
+reg[31:0] result;
+wire [31:0] sub_result = alu_a - alu_b;
+
+always @(alu_op or alu_a or alu_b or sub_result)
+begin
+	case (alu_op)
+		`ALU_SHIfTL:
+			result = alu_a << alu_b;
+		`ALU_SHIFTR:
+			result = alu_a >> alu_b;
+		`ALU_ADD:
+			result = (alu_a + alu_b);
+		`ALU_SUB:
+			result = (alu_a - alu_b);
+		`ALU_AND:
+			result = (alu_a & alu_b);
+		`ALU_OR:
+			result = (alu_a | alu_b);
+		`ALU_XOR:
+			result = (alu_a ^ alu_b);
+		`ALU_LESS_THAN:
+			result = (alu_a < alu_b) ? 32'1 : 32'h0;
+		`ALU_LESS_THAN_SIGNED:
+		begin
+			if(alu_a[31] != alu_b[31])
+				result = alu_a[31] ? 32'h1 : 32'h0;
+			else
+				result = sub_result = sub_result[31] ? 32'h1: 32'h0;
+		end
+		default:
+			result = alu_a;
+	endcase
+end
+
+alu_out = result;
 
 endmodule
---
+++ riscv_defs.v
@@ -0,0 +1,13 @@
+// ALU Operations
+
+`define ALU_NONE				4'b0000
+`define ALU_SHIFTL				4'b0001
+`define ALU_SHIFTR				4'b0010
+`define ALU_SHIFTR_ARITH		4'b0011
+`define ALU_ADD					4'b0100
+`define ALU_SUB					4'b0110
+`define ALU_AND					4'b0111
+`define ALU_OR					4'b1000
+`define ALU_XOR					4'b1001
+`define ALU_LESS_THAN			4'b1010
+`define ALU_LESS_THAN_SIGNED	4'b1011
